8c2cf1ee21cd3864b5c84e4ab927eb786766e6a1
Khloptsev Petr <petr.khloptsev@fogstream.ru>
2025-08-06 15:54:16 +1000

    Добавлен каркас модели острова с клеткой и позицией

--- src/main/java/org/khloptsev/island/Island.java
+++ src/main/java/org/khloptsev/island/Island.java
@@ -1,5 +1,28 @@
 package org.khloptsev.island;
 
 public class Island {
+    private final int HEIGHT;
+    private final int WIDTH;
+    private final Cell[][] GRID;
 
+    public Island(int height, int width) {
+        HEIGHT = height;
+        WIDTH = width;
+        GRID = new Cell[height][width];
+
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                GRID[y][x] = new Cell(new Position(x, y));
+            }
+        }
+    }
+
+    public void printIsland() {
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                System.out.print("[" + GRID[y][x] + "] ");  // пока пустая клетка
+            }
+            System.out.println();
+        }
+    }
 }
